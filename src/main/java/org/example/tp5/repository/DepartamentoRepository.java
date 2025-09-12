d734c596606a8b72192def6035522b704c356b39
meschinidino <francodmsoloing@gmail.com>
2025-09-12 20:09:49 -0300

    se crean repositories

---
+++ src/main/java/org/example/tp5/repository/DepartamentoRepository.java
@@ -0,0 +1,9 @@
+package org.example.tp5.repository;
+
+import org.example.tp5.model.Departamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartamentoRepository extends JpaRepository<Departamento, Long> {
+}
